1c2acd56ddcae342ac6612cecf6ada3f07d10e83
zmrowicki@hotmail.com <zmrowicki@hotmail.com>
2016-05-28 16:22:41 -0700

    Add form validators with flask-wtf. Add forms.py

--- project/views.py
+++ project/views.py
@@ -1,8 +1,8 @@
 import sqlite3
 from functools import wraps
-
 from flask import Flask, flash, redirect, render_template, request, \
         session, url_for, g
+from forms import AddTaskForm
 
 # Config
 app = Flask(__name__)
